1d4d286ba4a287ca8dd53ab2e653886ea1c2221a
Vishal Shinde <vishalshinde@541@gmail.com>
2024-02-14 12:20:09 +0530

    Created app 'Happy Birthday' using Jetpack compose using for UI

---
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.happybirthday
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.happybirthday.ui.theme.HappyBirthdayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HappyBirthdayTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingText("Happy Birthday Akash", "From Vishal")
+                }
+            }
+        }
+    }
+}
+@Composable
+fun GreetingText(name: String,from: String, modifier: Modifier = Modifier){
+    Surface(color = Color.Cyan) {
+        Column(verticalArrangement = Arrangement.Center,
+            modifier = modifier, content = {
+                Text(
+                    text = name,
+                    fontSize = 80.sp,
+                    lineHeight = 96.sp,
+                    textAlign = TextAlign.Center
+                )
+
+                Text(
+                    text = from,
+                    fontSize = 36.sp,
+                    lineHeight = 48.sp,
+                    modifier = modifier
+                        .padding(16.dp)
+                        .align(Alignment.End)
+                )
+            })
+    }
+}
+
+
+@Preview(showBackground = true,
+    showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    HappyBirthdayTheme {
+        GreetingText("Happy Birthday Akash!", "From Vishal")
+    }
+}
